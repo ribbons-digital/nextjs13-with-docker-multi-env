ede2b069852f6fbbd013519ca20c0c2b7fb663b6
ribbons-digital <develop@ribbons.digital>
2023-03-12 09:56:01 +1100

    Add docker configs, prettier config and tailwindcss

---
+++ pages/test/index.tsx
@@ -0,0 +1,8 @@
+import "../../globals.css"
+export default function Test() {
+  return (
+    <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      Hello world!!
+    </button>
+  )
+}
